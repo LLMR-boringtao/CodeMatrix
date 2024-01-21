92a95d67140e33e5d43c85f668057c90960e6383
LLMR_Boringtao <138439682+LLMR-boringtao@users.noreply.github.com>
2024-01-21 23:14:45 +0000

    add: pdm auth

--- backend/run.py
+++ backend/run.py
@@ -2,15 +2,16 @@ import sys
 from pathlib import Path
 sys.path.append(str(Path(__file__).resolve().parent.parent))
 
-from quart import Quart
 from quart import Quart, Response, ResponseReturnValue
+from quart_auth import QuartAuth
 from backend.src.backend.blueprints.control import blueprint as control_blueprint
 from backend.src.backend.lib.api_error import APIError
- 
+
+
 app = Quart(__name__)
 app.config.from_prefixed_env(prefix="CodeMatrix")
 app.register_blueprint(control_blueprint)
-
+auth_manager = QuartAuth(app)
 
 @app.errorhandler(APIError)  # type: ignore
 async def handle_api_error(error: APIError) -> ResponseReturnValue:
