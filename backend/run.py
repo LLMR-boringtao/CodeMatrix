24097ad05cf66a840fc00931d6a8411466a2bc1b
LLMR_Boringtao <138439682+LLMR-boringtao@users.noreply.github.com>
2024-01-22 00:02:20 +0000

    add: rate limit

--- backend/run.py
+++ backend/run.py
@@ -4,6 +4,9 @@ sys.path.append(str(Path(__file__).resolve().parent.parent))
 
 from quart import Quart, Response, ResponseReturnValue
 from quart_auth import QuartAuth
+from quart_rate_limiter import RateLimiter
+from quart_rate_limiter import RateLimitExceeded
+
 from backend.src.backend.blueprints.control import blueprint as control_blueprint
 from backend.src.backend.lib.api_error import APIError
 
@@ -12,11 +15,18 @@ app = Quart(__name__)
 app.config.from_prefixed_env(prefix="CodeMatrix")
 app.register_blueprint(control_blueprint)
 auth_manager = QuartAuth(app)
+rate_limiter = RateLimiter(app)
 
-@app.errorhandler(APIError)  # type: ignore
+@app.errorhandler(APIError)
 async def handle_api_error(error: APIError) -> ResponseReturnValue:
     return {"code": error.code}, error.status_code
 
 @app.errorhandler(500)
 async def handle_generic_error(error: Exception) -> ResponseReturnValue:
     return {"code": "INTERNAL_SERVER_ERROR"}, 500
+ 
+@app.errorhandler(RateLimitExceeded)
+async def handle_rate_limit_exceeded_error(
+    error: RateLimitExceeded,
+) -> ResponseReturnValue:
+    return {}, error.get_headers(), 429
